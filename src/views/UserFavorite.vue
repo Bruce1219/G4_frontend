cfdd6b25d6325d21c13db67134948cbe6a56f581
elaine0623 <0148004@gmail.com>
2024-07-11 15:15:58 +0800

    收藏商品

--- src/views/UserFavorite.vue
+++ src/views/UserFavorite.vue
@@ -45,7 +45,7 @@ export default {
         p_noList: selectedItems,
         type: 2 // 添加到購物車
       };
-      fetch(`http://localhost/php_g4/userFavoriteUpdate`, {
+      fetch(`${import.meta.env.VITE_API_URL}/userFavoriteUpdate`, {
         method: "POST",
         body: JSON.stringify(body)
       })
@@ -61,7 +61,7 @@ export default {
         return;
       }
 
-      fetch('http://localhost/php_G4/userFavorite.php', {
+      fetch(`${import.meta.env.VITE_API_URL}userFavorite`, {
         method: 'POST',
         body: JSON.stringify({ m_no: this.m_no })
       })
