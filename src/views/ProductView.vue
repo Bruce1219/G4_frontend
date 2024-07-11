ba21e3a31c6edd7f892163dd9663ff00f271640f
leona1128 <aerial22510@gmail.com>
2024-07-11 18:28:29 +0800

    刪除ASSETS

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -199,7 +199,7 @@ export default {
       }
     },
     parsePic(file) {
-      return `${import.meta.env.VITE_FILE_URL}assets/${file}`
+      return `${import.meta.env.VITE_FILE_URL}${file}`
     },
     addCart(id) {
       const targetItem = this.responseData.find(v => v.id === id);
